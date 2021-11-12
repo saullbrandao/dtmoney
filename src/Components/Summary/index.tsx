66f1193d2648353413402c8ced73212de7b01cb6
Saull Brandão <saull@outlook.com>
2021-11-12 16:57:48 -0300

    feat: get summary data from the Transactions context

--- src/Components/Summary/index.tsx
+++ src/Components/Summary/index.tsx
@@ -2,8 +2,35 @@ import * as S from './styles'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
+import { useTransactions } from '../../hooks/useTransactions'
 
 export const Summary = () => {
+  const { transactions } = useTransactions()
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      switch (transaction.type) {
+        case 'deposit':
+          acc.income += transaction.amount
+          acc.total += transaction.amount
+          break
+        case 'withdraw':
+          acc.outcome += transaction.amount
+          acc.total -= transaction.amount
+          break
+        default:
+          break
+      }
+
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
+
   return (
     <S.Container>
       <S.SummaryBox>
@@ -11,21 +38,37 @@ export const Summary = () => {
           <p>Income</p>
           <img src={incomeImg} alt="Income" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.income)}
+        </strong>
       </S.SummaryBox>
       <S.SummaryBox>
         <header>
           <p>Outcome</p>
           <img src={outcomeImg} alt="Outcome" />
         </header>
-        <strong>-R$500,00</strong>
+        <strong>
+          -
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.outcome)}
+        </strong>
       </S.SummaryBox>
       <S.SummaryBox className="highlight-bg">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.total)}
+        </strong>
       </S.SummaryBox>
     </S.Container>
   )
